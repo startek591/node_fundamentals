fa0da731071b6fbf111eddfcacc67d6089e87e81
jonathan_fambro <startek591@gmail.com>
2023-08-25 15:38:48 -0400

    Communication from the terminal to the client and the client to terminal

---
+++ 02-async/17-ee-create-emitter.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const readline = require('readline');
+const querystring = require('querystring');
+const EventEmitter = require('events');
+
+const rl = readline.createInterface({ input: process.stdin });
+
+rl.on('line', line => {
+  http.get(
+    `http://localhost:1337/chat?${querystring.stringify({ message: line })}`
+  );
+});
+
+const source = createEventSource('http://localhost:1337/sse');
+source.on('message', console.log);
+
+function createEventSource(url) {
+  const source = new EventEmitter();
+
+  http.get(url, res => {
+    res.on('data', data => {
+      const message = data
+        .toString()
+        .replace(/^data: /, '')
+        .replace(/\n\n$/, '');
+
+      source.emit('message', message);
+    });
+  });
+  return source;
+}
