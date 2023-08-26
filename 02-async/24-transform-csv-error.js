4be408c7db0e210d4c1663985296e29996d7e4ac
jonathan_fambro <startek591@gmail.com>
2023-08-26 16:29:55 -0400

    Error

---
+++ 02-async/24-transform-csv-error.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const { Transform } = require('stream');
+
+fs.createReadStream('people.csv')
+  .pipe(csv())
+  .pipe(clean())
+  .on('data', row => console.log(JSON.stringify(row)));
+
+function clean() {
+  return new Transform({
+    transform(row, encoding, callback) {
+      callback(null, row);
+    },
+  });
+}
