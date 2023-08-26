118052052a4e3f47d653eed029223680e6cf9a61
jonathan_fambro <startek591@gmail.com>
2023-08-26 16:07:33 -0400

    write a chunk of data to a disk when it comes in

---
+++ 02-async/20-streams-download-book-cover-write-stream.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const https = require('https');
+
+const fileUrl =
+  'https://www.newline.co/fullstack-react/assets/images/fullstack-react-hero-book.png';
+
+https.get(fileUrl, res => {
+  const fileStream = fs.createWriteStream('book.png');
+  res
+    .on('data', data => fileStream.write(data))
+    .on('end', () => {
+      fileStream.end();
+      console.log('file saved!');
+    });
+});
