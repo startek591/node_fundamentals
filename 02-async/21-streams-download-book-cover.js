dd2bfb3d58f0a70a38493ae1f2c6d3072dac9dca
jonathan_fambro <startek591@gmail.com>
2023-08-26 16:14:33 -0400

    Composing Streams

---
+++ 02-async/21-streams-download-book-cover.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const https = require('https');
+
+const fileUrl =
+  'https://www.newline.co/fullstack-react/assets/images/fullstack-react-hero-book.png';
+
+https.get(fileUrl, res => {
+  res
+    .pipe(fs.createWriteStream('book.png'))
+    .on('finish', () => console.log('file saved!'));
+});
