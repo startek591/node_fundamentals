0907b11dc391fc6fa89307cbe8532f1611d5b501
jonathan_fambro <startek591@gmail.com>
2023-08-27 13:31:41 -0400

    Validation Functionality works

--- 04-persistence/products.js
+++ 04-persistence/products.js
@@ -4,12 +4,12 @@ const db = require('./db');
 
 const Product = db.model('Product', {
   _id: { type: String, default: cuid },
-  description: String,
-  imgThumb: String,
-  img: String,
+  description: { type: String, required: true },
+  imgThumb: { type: String, required: true },
+  img: { type: String, required: true },
   link: String,
-  userId: String,
-  userName: String,
+  userId: { type: String, required: true },
+  userName: { type: String, required: true },
   userLink: String,
   tags: { type: [String], index: true },
 });
