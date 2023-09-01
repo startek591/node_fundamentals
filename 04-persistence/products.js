d2042bdf031798601644c1454b1f733eddc6eba5
jonathan_fambro <startek591@gmail.com>
2023-08-31 21:20:44 -0400

    Validation

--- 04-persistence/products.js
+++ 04-persistence/products.js
@@ -1,16 +1,17 @@
 const cuid = require('cuid');
+const { isURL } = require('validator');
 
 const db = require('./db');
 
 const Product = db.model('Product', {
   _id: { type: String, default: cuid },
   description: { type: String, required: true },
-  imgThumb: { type: String, required: true },
-  img: { type: String, required: true },
-  link: String,
+  imgThumb: urlSchema({ required: true }),
+  img: urlSchema({ required: true }),
+  link: urlSchema(),
   userId: { type: String, required: true },
   userName: { type: String, required: true },
-  userLink: String,
+  userLink: urlSchema(),
   tags: { type: [String], index: true },
 });
 
@@ -59,4 +60,16 @@ async function edit(_id, change) {
 
 async function remove(_id) {
   await Product.deleteOne({ _id });
+}
+
+function urlSchema(opts = {}) {
+  const { required } = opts;
+  return {
+    type: String,
+    required: required,
+    validate: {
+      validator: isURL,
+      message: props => `${props.value} is not a valid URL`,
+    },
+  };
 }
